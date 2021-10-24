f97ea6c38c296dba8383a3e56c5e666f238306d4
anujk07 <anujk66423@gmail.com>
2021-10-25 00:26:39 +0530

    film

---
+++ film.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int a,b,c;
+
+cout<<"enter the value of a:";
+
+cin>>a;
+
+cout<<"enter the vaue of b:";
+
+cin>>b;
+ 
+ c=a+b;
+
+ cout<<c;
+
+ return 0;
+ 
+}
+
